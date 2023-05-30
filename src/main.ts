ffb4a68e6fa77ed50c578163d393a199405213b6
Kai <kai.kasa@uniguest.com>
2023-05-30 21:17:21 +0200

    Install and set up Quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'quasar/dist/quasar.sass'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+import { Quasar } from 'quasar'
+
+App.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
 
 createApp(App).mount('#app')
